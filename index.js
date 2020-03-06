da8b42cbb16b106222788930473ee12c5e11141c
Matthias Evering <36561948+ewingson@users.noreply.github.com>
2020-03-06 16:47:37 +0100

    parameters, indentation
    d2020-03-06 t16:47

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 //definition of the function
 function nonsense(test)
 {
-//set debug parameter to string "on"
-debug = "on";
-//instructions to check log directory
-console.log ("check /var/log");
-//set value parameter to input parameter
-value = ("test:" + test)
-//output testvalue
-console.log (value);
-//return testvalue
-return value
+  //set dummy
+  var dummy = "";
+  //set flag
+  var truth = false;
+  //set integer test
+  var wert = 0;
+  //set debug parameter to string "on"
+  var debug = "on";
+  //instructions to check log directory
+  console.log ("check /var/log");
+  //set value parameter to input parameter
+  var value = ("test:" + test);
+  //output testvalue
+  console.log (value);
+  //return testvalue
+  return value;
 };
 //set export
 module.exports = nonsense;
