fe6d807060f9842c473db1df3002a3aa0edbcc34
Matthias Evering <36561948+ewingson@users.noreply.github.com>
2020-03-06 17:05:05 +0100

    silent
    d2020-03-06 t17:04

--- index.js
+++ index.js
@@ -9,14 +9,16 @@ function nonsense(test)
   var wert = 0;
   //set debug parameter to string "on"
   var debug = "on";
+  //set mode
+  var mode = "silent";
   //instructions to check log directory
   console.log ("check /var/log");
   //set value parameter to input parameter
   var value = ("test:" + test);
   //output testvalue
   console.log (value);
-  //return testvalue
-  return value;
+  //return flag
+  return truth;
 };
 //set export
 module.exports = nonsense;
